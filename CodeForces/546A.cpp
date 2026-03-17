6eeceb4757c6f4ac3b1d3437b8ece5e14f564890
Avneesh Sharma <161005348+AvneeshS11@users.noreply.github.com>
2026-03-17 21:17:10 +0530

    17Mar2026

--- CodeForces/546A.cpp
+++ CodeForces/546A.cpp
@@ -4,5 +4,16 @@ using namespace std;
 
 int main()
 {
-    int a = 6;
+    int k, n, w;
+
+    cin >> k >> n >> w;
+    int cost = 0;
+    for (int i = 1; i <= w; i++)
+    {
+        cost += (k * i);
+    }
+    if (cost <= n)
+        cout << 0;
+    else
+        cout << cost - n;
 }
